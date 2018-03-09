76663d52389e54fa6bb1384a2077c1538143f1e4
Sitsofe Wheeler <sitsofe@yahoo.com>
2018-03-09 13:19:04 +0000

    hack

--- triggerfiohang.sh
+++ triggerfiohang.sh
@@ -1,1 +1,2 @@
 for i in {1..100}; do echo "Loop $i; $(date)"; ./fio hang.fio; if [[ $? -ne 0 ]]; then break; fi; done
+for i in {1..150}; do echo "Loop $i; $(date)"; ./fio --eta=always hang.fio; if [[ $? -ne 0 ]]; then break; fi; done
