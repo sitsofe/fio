dbfa3360e6b0a93f316917fae7043e49d84d1f23
Sitsofe Wheeler <sitsofe@yahoo.com>
2018-03-12 10:17:46 +0000

    final hack

--- signalhang.c
+++ signalhang.c
@@ -1,30 +1,49 @@
 /* Program that demonstrates a hang in pthread_cond_signal() when using
- * winpthreads. For Windows compile with
- * x86_64-w64-mingw32-gcc -g -O3 -Wall -static -pthread signalhang.c -o signalhang
+ * winpthreads (at least versions 5.0.2-7.2.0). On Windows compile with
+ * x86_64-w64-mingw32-gcc -g -O3 -static -pthread signalhang.c -o signalhang
  * on Linux compile with
- * gcc -g -O3 -Wall -lrt -pthread signalhang.c -o signalhang
+ * gcc -g -O3 -lrt -pthread signalhang.c -o signalhang
+ *
+ * Extra compilation options:
+ * -DDETECT_DEADLOCK=1 : Print a message and trigger an assert if it looks like
+ *                       threads have become deadlocked
+ * -DLOGGING=1 : Print logging information when wakes are done. "hits" is
+ *               incremented when a wake is done and there were two more other
+ *               threads waiting on the condition variable.
+ * -DNDEBUG : Disable all return code checking asserts and don't trigger an
+ *            assert after the deadlock is detected when using
+ *            DETECT_DEADLOCK=1
  *
  * Usage:
  * ./signalhang [THREAD_COUNT]
- * e.g.
- * PS C:\> $LASTEXITCODE=0; While ($LASTEXITCODE -eq 0) { signalhang.exe 64 }
+ * Program will print a done line with some stats on success or hang on
+ * deadlock. It is easy to loop runs until a failure (especially when using
+ * DETECT_DEADLOCK=1) by using the shell. For example:
+ * PS C:\> $LASTEXITCODE=0; While ($LASTEXITCODE -eq 0) { .\signalhang.exe 4 }
  * or
- * $ ok=0; while [[ ok -eq 0 ]]; do ./a.out 64; ok=$?; done
+ * $ rc=0; while [[ rc -eq 0 ]]; do ./signalhang 4; rc=$?; done
  *
- * NB: Deadlock does not show:
- * - On Linux with glibc
- * - When less than 4 threads are used
- * - When all the contending threads are bound to the same CPU under native
- *   Windows
+ * Deadlock does not occur when any of the following are true:
+ * - Running on native Linux with glibc
+ * - When the call to pthread_cond_signal() is moved inside the
+ *   pthread_mutex_lock()/pthread_mutex_unlock() region
+ * The following make deadlock less likely:
+ * - Binding threads to the same single CPU
+ * - Using less than 4 threads
+ * The following make deadlock more likely:
+ * - More threads
+ * - Turning logging on
+ * - Calling sched_yield() between mutex_down()/mutex_up()
+ * - Binding threads to different CPUs
  */
 
 #ifndef LOGGING
 #define LOGGING 0 // setting to 1 increases deadlock probability
 #endif
 #ifndef DETECT_DEADLOCK
-#define DETECT_DEADLOCK 0
+#define DETECT_DEADLOCK 0 // setting to 1 enables self deadlock detection
 #endif
-#define MAX_MISSED_HEARTBEATS 5
+#define MAX_MISSED_HEARTBEATS 10 // the max missed heartbeats with deadlock detection
 
 #ifdef _WIN32
 #include <windows.h>
@@ -39,7 +58,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define CHECK(x) assert(x == 0)
+#define CHECK(x) if ((x) != 0) assert(0)
 
 #if LOGGING
 #define log(x, ...) fprintf(stderr, x, ##__VA_ARGS__)
@@ -71,14 +90,14 @@ struct sh_thread {
 };
 
 static void mutex_down(struct sh_mutex *mutex) {
-	pthread_mutex_lock(&mutex->lock);
+	CHECK(pthread_mutex_lock(&mutex->lock));
 	while (mutex->value == MUTEX_LOCKED) {
 		mutex->waiters++;
-		pthread_cond_wait(&mutex->cond, &mutex->lock);
+		CHECK(pthread_cond_wait(&mutex->cond, &mutex->lock));
 		mutex->waiters--;
 	}
 	mutex->value = MUTEX_LOCKED;
-	pthread_mutex_unlock(&mutex->lock);
+	CHECK(pthread_mutex_unlock(&mutex->lock));
 }
 
 static void mutex_up(struct sh_mutex *mutex) {
@@ -86,7 +105,7 @@ static void mutex_up(struct sh_mutex *mutex) {
 	uint64_t iteration;
 	uint64_t hits;
 
-	pthread_mutex_lock(&mutex->lock);
+	CHECK(pthread_mutex_lock(&mutex->lock));
 	if (mutex->value == MUTEX_LOCKED && mutex->waiters)
 		do_wake = mutex->waiters;
 	mutex->value = MUTEX_UNLOCKED;
@@ -94,11 +113,11 @@ static void mutex_up(struct sh_mutex *mutex) {
 	if (do_wake && mutex->waiters > 1)
 		mutex->hits++;
 	hits = mutex->hits;
-	pthread_mutex_unlock(&mutex->lock);
+	CHECK(pthread_mutex_unlock(&mutex->lock));
 
 	if (do_wake) {
 		log("doing wake (waiters=%d, iteration=%" PRId64 ", hits=%" PRId64 ")...\n", do_wake, iteration, hits);
-		pthread_cond_signal(&mutex->cond);
+		CHECK(pthread_cond_signal(&mutex->cond));
 	}
 }
 
@@ -110,7 +129,7 @@ static int cpu_bind(int cpu) {
 
 	if (SetThreadAffinityMask(GetCurrentThread(), mask) == 0) {
 		log("GetLastError=%ld\n", GetLastError());
-		assert(1);
+		assert(0);
 	}
 #else
 	cpu_set_t cpuset;
@@ -125,13 +144,27 @@ static int cpu_bind(int cpu) {
 	return 0;
 }
 
+static int get_online_cpus(void) {
+	int cpus;
+#ifdef _WIN32
+	SYSTEM_INFO sysinfo;
+
+	GetSystemInfo(&sysinfo);
+	cpus = sysinfo.dwNumberOfProcessors;
+#else
+	cpus = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+	return cpus;
+}
+
 static void *contend_lock_thread(void *data) {
 	struct sh_thread *thread_data = (struct sh_thread*) data;
 	struct sh_mutex	*mutex = thread_data->mutex;
 	int cpu;
 
 	//cpu = 0; // bind all threads to the first CPU
-	cpu = thread_data->id % 2; // bind thread to ONE of the first 2 CPUs
+	//cpu = thread_data->id % 2; // bind each thread to ONE of the first 2 CPUs
+	cpu = thread_data->id % get_online_cpus(); // bind threads to individual CPUs
 	CHECK(cpu_bind(cpu));
 	while (1) {
 		uint64_t iteration, hits;
@@ -168,14 +201,13 @@ int main(int argv, char **argc) {
 	CHECK(pthread_mutex_init(&mutex.lock, NULL));
 	mutex.value = MUTEX_UNLOCKED;
 
-	/* NB: Problem does not show with thread_count < 4 */
 	if (argv > 1) {
 		thread_count = atoi(argc[1]);
 	} else
 		thread_count = 8;
 
 	mutex.max_iterations = 100000000;
-	mutex.max_hits = 10000;
+	mutex.max_hits = 100000;
 
 	no_heartbeat = 1;
 	alive = thread_count;
@@ -193,21 +225,26 @@ int main(int argv, char **argc) {
 
 #if DETECT_DEADLOCK
 	int missed_heartbeats = 0;
+	int missed_heartbeats_ever = 0;
 	while (1) {
 		int local_alive;
 		unsigned int no_new_heartbeat;
 
-		sleep(1);
 		local_alive = __sync_fetch_and_add(&alive, 0);
-		if (local_alive) {
-			no_new_heartbeat = __sync_lock_test_and_set(&no_heartbeat, 1);
-			if (no_new_heartbeat) {
-				missed_heartbeats++;
-				fprintf(stderr, "heartbeat missed\n");
-			}
+		if (!local_alive)
+			break;
+
+		usleep(500000);
+		no_new_heartbeat = __sync_lock_test_and_set(&no_heartbeat, 1);
+		if (no_new_heartbeat) {
+			missed_heartbeats++;
+			missed_heartbeats_ever++;
+			fprintf(stderr, "heartbeat missed\n");
+			fflush(stderr);
 
 			if (missed_heartbeats >= MAX_MISSED_HEARTBEATS) {
 				fprintf(stderr, "deadlock detected! missed_heartbeats=%d alive=%d\n", missed_heartbeats, local_alive);
+				fflush(stderr);
 #ifdef NDEBUG
 				break;
 #else
@@ -215,7 +252,7 @@ int main(int argv, char **argc) {
 #endif
 			}
 		} else
-			break;
+			missed_heartbeats = 0;
 	}
 #endif
 
@@ -228,5 +265,8 @@ int main(int argv, char **argc) {
 		free(threads_data);
 
 	printf("done: iterations=%" PRId64 ", hits=%" PRId64 "\n", mutex.iteration, mutex.hits);
+#if DETECT_DEADLOCK
+	printf("      missed_heartbeats_ever=%d\n", missed_heartbeats_ever);
+#endif
 	return 0;
 }
