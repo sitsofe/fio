2c6807e4860b5c71a70eebbf3991b9e3918b6aee
Sitsofe Wheeler <sitsofe@yahoo.com>
2021-12-11 11:37:46 +0000

    ci: more debug

--- ci/actions-install.sh
+++ ci/actions-install.sh
@@ -31,7 +31,7 @@ DPKGCFG
     case "${CI_TARGET_ARCH}" in
         "i686")
             sudo dpkg --add-architecture i386
-            dpkg -l *deb.sury.org*
+            dpkg -l | grep '.*deb.sury.org.*'
             sudo apt-get remove -y php7.4 php8.0 php7.4-cgi php8.0-cgi php8.1-fpm php8.1-cgi php8.1-phpdbg libselinux1 php8.1-cli php8.0-phpdbg php7.4-phpdbg 
             pkgs=("${pkgs[@]/%/:i386}")
             pkgs+=(
